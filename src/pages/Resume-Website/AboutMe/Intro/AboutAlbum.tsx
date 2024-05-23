9c18d3a5a4f3eca037ac1536ac7411da24f15695
Ejmen-Ramic-DriveFlux <ejmen@driveflux.com>
2024-05-23 15:49:21 +0800

    About album updated

--- src/pages/Resume-Website/AboutMe/Intro/AboutAlbum.tsx
+++ src/pages/Resume-Website/AboutMe/Intro/AboutAlbum.tsx
@@ -17,10 +17,10 @@ const AboutAlbum: FC = () => {
       <Flex w={'full'} maxH={{ lg: '1200x' }} direction={{ base: 'column', lg: 'row' }}>
         <VStack w={'full'} justify={'space-between'} spacing={{ base: '50px', lg: '0px' }}>
           <FadeInView delay={0.1}>
-            <Image src={'./Website/Resume/AboutMe/aboutintro1.jpg'} alt={'About Img 1'} sx={zoomInStyles} />
+            <Image src={'./Website/Resume/AboutMe/about-me-1.jpg'} alt={'About Img 1'} sx={zoomInStyles} />
           </FadeInView>{' '}
           <FadeInView delay={0.1}>
-            <Image src={'./Website/Resume/AboutMe/aboutintro2.jpg'} alt={'About Img 2'} sx={zoomInStyles} />
+            <Image src={'./Website/Resume/AboutMe/about-me-2.jpg'} alt={'About Img 2'} sx={zoomInStyles} />
           </FadeInView>
         </VStack>
         <Box w={'full'} mt={{ base: '50px', lg: '0px' }} ml={{ base: '0px', lg: '50px' }} position={'relative'}>
