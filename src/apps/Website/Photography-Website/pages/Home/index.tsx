99a7bf9e8dff91bc3fae3047c144da327cbca3c3
Ejmen-Ramic-DriveFlux <ejmen@driveflux.com>
2024-05-28 11:00:32 +0800

    Update

--- src/apps/Website/Photography-Website/pages/Home/index.tsx
+++ src/apps/Website/Photography-Website/pages/Home/index.tsx
@@ -10,8 +10,8 @@ const PhotographyHome = () => {
     <Stack w={'full'} bgColor={useColorModeValue('#ede9e3', '#2b333d')}>
       <HeaderPhotography />
       <SimpleGrid columns={{ base: 1, lg: 2 }} spacing={{ base: '0px', lg: '20px' }} mb={'100px'}>
-        <FadeInView>
-          {itemProps.map(({ image, location, link }, i) => (
+        {itemProps.map(({ image, location, link }, i) => (
+          <FadeInView>
             <Stack
               as={Link}
               key={i}
@@ -27,24 +27,29 @@ const PhotographyHome = () => {
               justify={'center'}
               textAlign={'left'}
               zIndex={1}
+              style={{
+                backdropFilter: 'blur(0.5rem)', // Adjust blur strength as needed
+              }}
             >
               {link}
-              <Flex
-                letterSpacing={'2px'}
-                fontSize={'26px'}
-                fontWeight={400}
-                color={'white'}
-                px={'80px'}
-                pt={'75px'}
-                direction={'column'}
-                alignItems={'flex-start'}
-                zIndex={10}
-              >
-                <Text textTransform={'uppercase'}>{location}</Text>
-              </Flex>
+              <FadeInView delay={0.4} direction={'left'}>
+                <Flex
+                  letterSpacing={'2px'}
+                  fontSize={'26px'}
+                  fontWeight={400}
+                  color={'white'}
+                  px={'80px'}
+                  pt={'75px'}
+                  direction={'column'}
+                  alignItems={'flex-start'}
+                  zIndex={10}
+                >
+                  <Text textTransform={'uppercase'}>{location}</Text>
+                </Flex>
+              </FadeInView>
             </Stack>
-          ))}
-        </FadeInView>
+          </FadeInView>
+        ))}
       </SimpleGrid>
       <Footer />
     </Stack>
