19be6dc3a5c0a815c2a4ef89adf137cc71ccbfd2
Ejmen-Ramic-DriveFlux <ejmen@driveflux.com>
2024-08-28 12:40:12 +0800

    Biome config + EmailJs full implementation

--- src/apps/Website/Resume-Website/pages/Contacts/Contact.tsx
+++ src/apps/Website/Resume-Website/pages/Contacts/Contact.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import type React from 'react'
 
 import { Stack } from '@chakra-ui/react'
 import ContactForm from './ContactForm/ContactForm '
 import Header from '../../../../../shared/components/Header'
 import Footer from '../../../../../shared/components/Footer/Footer'
 
-const Contact: React.FC<{}> = () => {
-  return (
-    <>
-      <Header />
-      <Stack width={'full'}>
-        <ContactForm />
-      </Stack>
-      <Footer />
-    </>
-  )
+const Contact: React.FC = () => {
+	return (
+		<>
+			<Header />
+			<Stack width={'full'}>
+				<ContactForm />
+			</Stack>
+			<Footer />
+		</>
+	)
 }
 
 export default Contact
