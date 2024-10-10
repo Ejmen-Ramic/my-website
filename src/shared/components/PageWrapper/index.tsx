999ed52b40f98bbb4629bb37f68acea402fc1746
Ejmen Ramic <ejmen@driveflux.com>
2024-10-10 20:03:38 +0800

    Added conditions to Page component

--- src/shared/components/PageWrapper/index.tsx
+++ src/shared/components/PageWrapper/index.tsx
@@ -1,19 +1,20 @@
 import Footer from "./Footer/Footer"
 import Header from "./Header"
 import PageWrapper from "../Hooks/PageWrapper"
-import { Box, BoxProps, StackProps } from "@chakra-ui/react"
+import { Box, StackProps } from "@chakra-ui/react"
 
 interface PageWrapperProps extends StackProps {
     children: React.ReactNode
-
+    isFooterDisabled?: boolean
+    isHeaderDisabled?: boolean
 }
 
-const Page = ({children}: PageWrapperProps) => {
+const Page = ({children, isFooterDisabled = true, isHeaderDisabled = true}: PageWrapperProps) => {
     return(
         <PageWrapper display={'flex'} flexDirection={'column'} minH={'100vh'}>
-        <Header/>
+        {isHeaderDisabled && <Header/>}
         <Box flex={'1'}>{children}</Box>
-        <Footer />
+        {isFooterDisabled && <Footer />}
         </PageWrapper>
     )
 }
