ca51160687652da8a7d48ac12cadd6472774e029
Ejmen Ramic <ejmen@driveflux.com>
2024-10-29 13:00:47 +0800

    Commented test file

--- src/apps/Website/test.tsx
+++ src/apps/Website/test.tsx
@@ -1,76 +1,76 @@
-import {
-	Stepper,
-	Step,
-	StepIndicator,
-	StepStatus,
-	StepIcon,
-	StepNumber,
-	Box,
-	StepTitle,
-	StepDescription,
-	StepSeparator,
-	Button,
-	useSteps,
-} from "@chakra-ui/react";
+// import {
+// 	Stepper,
+// 	Step,
+// 	StepIndicator,
+// 	StepStatus,
+// 	StepIcon,
+// 	StepNumber,
+// 	Box,
+// 	StepTitle,
+// 	StepDescription,
+// 	StepSeparator,
+// 	Button,
+// 	useSteps,
+// } from "@chakra-ui/react";
 
-const steps = [
-	{ title: "First", description: "Contact Info" },
-	{ title: "Second", description: "Date & Time" },
-	{ title: "Third", description: "Select Rooms" },
-];
+// const steps = [
+// 	{ title: "First", description: "Contact Info" },
+// 	{ title: "Second", description: "Date & Time" },
+// 	{ title: "Third", description: "Select Rooms" },
+// ];
 
-const Example = () => {
-	const { activeStep, setActiveStep } = useSteps({
-		index: 0, // start at the first step
-		count: steps.length,
-	});
+// const Example = () => {
+// 	const { activeStep, setActiveStep } = useSteps({
+// 		index: 0, // start at the first step
+// 		count: steps.length,
+// 	});
 
-	const handleNext = () => {
-		if (activeStep < steps.length - 1) {
-			setActiveStep(activeStep + 1);
-		}
-	};
+// 	const handleNext = () => {
+// 		if (activeStep < steps.length - 1) {
+// 			setActiveStep(activeStep + 1);
+// 		}
+// 	};
 
-	const handlePrevious = () => {
-		if (activeStep > 0) {
-			setActiveStep(activeStep - 1);
-		}
-	};
+// 	const handlePrevious = () => {
+// 		if (activeStep > 0) {
+// 			setActiveStep(activeStep - 1);
+// 		}
+// 	};
 
-	return (
-		<Box>
-			<Stepper size="lg" index={activeStep}>
-				{steps.map((step, index) => (
-					<Step key={step.title} onClick={() => setActiveStep(index)}>
-						<StepIndicator>
-							<StepStatus
-								complete={<StepIcon />}
-								incomplete={<StepNumber />}
-								active={<StepNumber />}
-							/>
-						</StepIndicator>
+// 	return (
+// 		<Box>
+// 			<Stepper size="lg" index={activeStep}>
+// 				{steps.map((step, index) => (
+// 					<Step key={step.title} onClick={() => setActiveStep(index)}>
+// 						<StepIndicator>
+// 							<StepStatus
+// 								complete={<StepIcon />}
+// 								incomplete={<StepNumber />}
+// 								active={<StepNumber />}
+// 							/>
+// 						</StepIndicator>
 
-						<Box flexShrink="0">
-							<StepTitle>{step.title}</StepTitle>
-							<StepDescription>{step.description}</StepDescription>
-						</Box>
+// 						<Box flexShrink="0">
+// 							<StepTitle>{step.title}</StepTitle>
+// 							<StepDescription>{step.description}</StepDescription>
+// 						</Box>
 
-						<StepSeparator />
-					</Step>
-				))}
-			</Stepper>
+// 						<StepSeparator />
+// 					</Step>
+// 				))}
+// 			</Stepper>
 
-			<Box mt={4}>
-				{activeStep > 0 && (
-					<Button onClick={handlePrevious} mr={4}>
-						Previous
-					</Button>
-				)}
-				<Button onClick={handleNext}>
-					{activeStep === steps.length - 1 ? "Finish" : "Next"}
-				</Button>
-			</Box>
-		</Box>
-	);
-};
-export default Example;
+// 			<Box mt={4}>
+// 				{activeStep > 0 && (
+// 					<Button onClick={handlePrevious} mr={4}>
+// 						Previous
+// 					</Button>
+// 				)}
+// 				<Button onClick={handleNext}>
+// 					{activeStep === steps.length - 1 ? "Finish" : "Next"}
+// 				</Button>
+// 			</Box>
+// 		</Box>
+// 	);
+// };
+// export default Example;
