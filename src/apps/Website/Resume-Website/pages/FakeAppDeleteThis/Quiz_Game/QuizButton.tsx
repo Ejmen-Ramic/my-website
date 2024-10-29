59d77bc53c45363250ce6c082c0cebc251c6e90a
Ejmen Ramic <ejmen@driveflux.com>
2024-10-29 14:13:15 +0800

    Commented the quiz game

--- src/apps/Website/Resume-Website/pages/FakeAppDeleteThis/Quiz_Game/QuizButton.tsx
+++ src/apps/Website/Resume-Website/pages/FakeAppDeleteThis/Quiz_Game/QuizButton.tsx
@@ -1,35 +1,35 @@
-import { Button } from '@chakra-ui/react'
-import { FC, MouseEventHandler } from 'react'
-import { colors } from '../../../../../../shared/components/Hooks/color'
+// import { Button } from '@chakra-ui/react'
+// import { FC, MouseEventHandler } from 'react'
+// import { colors } from '../../../../../../shared/components/Hooks/color'
 
-type QuizButtonProps = {
-  text: string
-  disabled?: boolean
-  onClick: MouseEventHandler<HTMLButtonElement>
-}
+// type QuizButtonProps = {
+//   text: string
+//   disabled?: boolean
+//   onClick: MouseEventHandler<HTMLButtonElement>
+// }
 
-const QuizButton: FC<QuizButtonProps> = ({ text, onClick, disabled }) => {
-  return (
-    <Button
-      disabled={disabled}
-      position={'relative'}
-      ml={'20px'}
-      size={'lg'}
-      h={'40px'}
-      w={'150px'}
-      _hover={{ bg: colors.black, color: colors.white }}
-      bg={colors.white}
-      color={colors.black}
-      border={'1px'}
-      borderRadius={'0px'}
-      borderColor={colors.black}
-      letterSpacing={'3px'}
-      fontSize={'12px'}
-      onClick={onClick}
-    >
-      {text}
-    </Button>
-  )
-}
+// const QuizButton: FC<QuizButtonProps> = ({ text, onClick, disabled }) => {
+//   return (
+//     <Button
+//       disabled={disabled}
+//       position={'relative'}
+//       ml={'20px'}
+//       size={'lg'}
+//       h={'40px'}
+//       w={'150px'}
+//       _hover={{ bg: colors.black, color: colors.white }}
+//       bg={colors.white}
+//       color={colors.black}
+//       border={'1px'}
+//       borderRadius={'0px'}
+//       borderColor={colors.black}
+//       letterSpacing={'3px'}
+//       fontSize={'12px'}
+//       onClick={onClick}
+//     >
+//       {text}
+//     </Button>
+//   )
+// }
 
-export default QuizButton
+// export default QuizButton
--- src/apps/Website/Resume-Website/pages/FakeAppDeleteThis/Quiz_Game/ResultCard.tsx
+++ src/apps/Website/Resume-Website/pages/FakeAppDeleteThis/Quiz_Game/ResultCard.tsx
@@ -1,65 +1,65 @@
-import { HStack, Box, Button, Divider, VStack } from '@chakra-ui/react'
-import React from 'react'
-import ProgressBar from './ProgressBar'
-import { colors } from '../../../../../../shared/components/Hooks/color'
+// import { HStack, Box, Button, Divider, VStack } from '@chakra-ui/react'
+// import React from 'react'
+// import ProgressBar from './ProgressBar'
+// import { colors } from '../../../../../../shared/components/Hooks/color'
 
-type ResultCardProps = {
-  correct: number
-  total: number
-  restartQuiz: () => void
-}
+// type ResultCardProps = {
+//   correct: number
+//   total: number
+//   restartQuiz: () => void
+// }
 
-const Results = {
-  Good: {
-    title: 'You are a car expert!!',
-    description:
-      'You have got the best answers! Based on your answers, our system detected that you are a car expert!!',
-  },
-  Bad: {
-    title: 'You are a bad driver!!',
-    description:
-      'You have got the worst answers! Based on your answers, our system detected that you are a bad driver!!',
-  },
-}
+// const Results = {
+//   Good: {
+//     title: 'You are a car expert!!',
+//     description:
+//       'You have got the best answers! Based on your answers, our system detected that you are a car expert!!',
+//   },
+//   Bad: {
+//     title: 'You are a bad driver!!',
+//     description:
+//       'You have got the worst answers! Based on your answers, our system detected that you are a bad driver!!',
+//   },
+// }
 
-const ResultCard: React.FC<ResultCardProps> = ({ correct, total, restartQuiz }) => {
-  const result = correct > 4 ? Results.Good : Results.Bad
-  return (
-    <VStack h={'230px'}>
-      <ProgressBar current={total} total={total} />
-      <HStack>
-        <Box fontSize={'25px'} fontWeight={'600'} color={'#20252f'}>
-          {result.title}
-        </Box>
-      </HStack>
-      <HStack>
-        <Box>{result.description}</Box>
-      </HStack>
-      <Divider />
-      <HStack>
-        <Box fontSize={'16px'} color={'#20252f'}>
-          You got {correct} out of {total} questions correct!
-        </Box>
-      </HStack>
-      <Divider />
-      <Button
-        size={'lg'}
-        h={'40px'}
-        w={'150px'}
-        _hover={{ bg: colors.white, color: colors.black }}
-        bg={colors.black}
-        color={colors.white}
-        border={'1px'}
-        borderRadius={'0px'}
-        borderColor={colors.black}
-        letterSpacing={'3px'}
-        fontSize={'12px'}
-        onClick={restartQuiz}
-      >
-        RESTART
-      </Button>
-    </VStack>
-  )
-}
+// const ResultCard: React.FC<ResultCardProps> = ({ correct, total, restartQuiz }) => {
+//   const result = correct > 4 ? Results.Good : Results.Bad
+//   return (
+//     <VStack h={'230px'}>
+//       <ProgressBar current={total} total={total} />
+//       <HStack>
+//         <Box fontSize={'25px'} fontWeight={'600'} color={'#20252f'}>
+//           {result.title}
+//         </Box>
+//       </HStack>
+//       <HStack>
+//         <Box>{result.description}</Box>
+//       </HStack>
+//       <Divider />
+//       <HStack>
+//         <Box fontSize={'16px'} color={'#20252f'}>
+//           You got {correct} out of {total} questions correct!
+//         </Box>
+//       </HStack>
+//       <Divider />
+//       <Button
+//         size={'lg'}
+//         h={'40px'}
+//         w={'150px'}
+//         _hover={{ bg: colors.white, color: colors.black }}
+//         bg={colors.black}
+//         color={colors.white}
+//         border={'1px'}
+//         borderRadius={'0px'}
+//         borderColor={colors.black}
+//         letterSpacing={'3px'}
+//         fontSize={'12px'}
+//         onClick={restartQuiz}
+//       >
+//         RESTART
+//       </Button>
+//     </VStack>
+//   )
+// }
 
-export default ResultCard
+// export default ResultCard
