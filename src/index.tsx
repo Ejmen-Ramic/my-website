b021170f7e30c407e26cbc24c6112ebe7b6837dc
Ejmen Ramic <ejmen@driveflux.com>
2024-10-29 09:09:08 +0800

    SRC Root 3.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import { ChakraProvider, theme } from '@chakra-ui/react'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { i18n } from '@lingui/core';
+import { I18nProvider } from '@lingui/react';
+import { messages } from './locales/en/messages';
+import { Provider } from './components/ui/provider';
 
-import { i18n } from '@lingui/core'
-import { I18nProvider } from '@lingui/react'
-import { messages } from './locales/en/messages'
-
-i18n.load('en', messages)
-i18n.activate('en')
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+i18n.load('en', messages);
+i18n.activate('en');
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
+    <Provider>
       <BrowserRouter>
         <I18nProvider i18n={i18n}>
           <App />
         </I18nProvider>
       </BrowserRouter>
-    </ChakraProvider>
+    </Provider>
   </React.StrictMode>
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
