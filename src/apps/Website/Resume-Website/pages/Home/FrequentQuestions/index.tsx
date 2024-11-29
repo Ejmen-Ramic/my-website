15ace2531590d3c26027364e78aec72dd973bbad
Ejmen Ramic <ejmenramic@Hawwas-MacBook-Air.local>
2024-11-29 20:15:51 +0800

    Updated the text in FAQ

--- src/apps/Website/Resume-Website/pages/Home/FrequentQuestions/index.tsx
+++ src/apps/Website/Resume-Website/pages/Home/FrequentQuestions/index.tsx
@@ -1,6 +1,6 @@
-import { FC, ReactNode } from 'react'
-import FAQDesktop from './FAQDesktop'
-import FAQMobile from './FAQMobile'
+import { FC, ReactNode } from 'react';
+import FAQDesktop from './FAQDesktop';
+import FAQMobile from './FAQMobile';
 import {
   Heading,
   VStack,
@@ -8,31 +8,46 @@ import {
   useBreakpointValue,
   Box,
   Stack,
-} from '@chakra-ui/react'
-import FadeInView from '../../../../../../shared/components/Hooks/FadeInView'
-import { t, Trans } from '@lingui/macro'
+} from '@chakra-ui/react';
+import FadeInView from '../../../../../../shared/components/Hooks/FadeInView';
+import { t, Trans } from '@lingui/macro';
 
 export type FAQItem = {
-  title: ReactNode
-  description: ReactNode
-}
+  title: ReactNode;
+  description: ReactNode;
+};
 
 const items: FAQItem[] = [
   {
     title: t`1. Tell me about yourself.`,
-    description: t`I am a software developer with a passion for creating intuitive and efficient user experiences. I have been working with latest languages, frameworks and testing tools for the past few years, and I love how these tools enable me to build scalable and maintainable applications. Outside of work, I enjoy hiking and exploring new places, which helps me stay creative and refreshed.`,
+    description: (
+      <Box>
+        <Trans>
+          I am a <b>software developer</b> with a passion for creating 
+          <b>intuitive</b> and 
+          <b>efficient user experiences</b>. I have been working with 
+          <b>latest languages, frameworks and testing tools</b> for the past few
+          years, and I love how these tools enable me to build <b>scalable</b> 
+          and <b>maintainable</b> applications.
+          <br />
+          <br /> Outside of work, I enjoy hiking and exploring new places, which
+          helps me stay creative and refreshed.
+        </Trans>
+      </Box>
+    ),
   },
   {
     title: t`2. What are your greatest strengths and weaknesses?`,
     description: (
       <Box>
         <Trans>
-          One of my greatest strengths is my ability to learn quickly and adapt
+          One of my greatest strengths is my ability to <b>learn quickly</b> and 
+          <b>adapt</b> 
           to new technologies. For instance, I picked up Chakra UI on my own and
-          have been able to integrate it seamlessly into my projects.
+          have been able to <b>integrate it seamlessly</b> into my projects.
           <br />
           <br /> On the flip side, a weakness of mine is that I can be a bit of
-          a perfectionist. Sometimes I spend too much time tweaking small
+          a <b>perfectionist</b>. Sometimes I spend too much time tweaking small
           details. I am working on balancing my attention to detail with
           efficiency.
         </Trans>
@@ -41,45 +56,113 @@ const items: FAQItem[] = [
   },
   {
     title: t`3. Where do you see yourself in five years?`,
-    description: t`In five years, I see myself taking on more leadership responsibilities, possibly leading a development team. I would like to continue honing my technical skills while also mentoring junior developers. I am also interested in exploring more about system architecture and design, contributing to larger strategic decisions.`,
+    description: (
+      <Box>
+        <Trans>
+          In five years, I see myself taking on more 
+          <b>leadership responsibilities</b>, possibly <b>leading</b> a
+          development team. I would like to continue honing my 
+          <b>technical skills</b> while also <b>mentoring</b> junior developers.
+          <br />
+          <br /> I am also interested in <b>exploring</b> more about system
+          architecture and design, <b>contributing</b> to larger strategic
+          decisions.
+        </Trans>
+      </Box>
+    ),
   },
   {
     title: t`4. Describe a challenging situation at work and how you handled it?`,
-    description: t`In one of my projects, we had a major issue, right before a product launch where our application was not scaling properly under load. I led a small team to identify the bottlenecks. We optimized our database queries and introduced caching mechanisms, which significantly improved performance. It was a stressful situation, but it taught me a lot about teamwork and problem-solving under pressure`,
+    description: (
+      <Box>
+        <Trans>
+          In one of my projects, we had a major issue, right before a product
+          launch where our application was not scaling properly under load. <b>I
+          led</b> a small team to identify the bottlenecks. <b>We optimized</b> our
+          database queries and <b>introduced</b> caching mechanisms, which
+          significantly <b>improved</b> performance. It was a stressful situation, but
+          it taught me a lot about <b>teamwork</b> and <b>problem-solving</b> under pressure.
+        </Trans>
+      </Box>
+    ),
   },
   {
     title: t`5. Give me an example of a time when you worked as part of a team.`,
-    description: t`I worked on a project where we had to develop a complex admin dashboard for the company. I was responsible for the frontend using React and Chakra UI and wrote tests using the Playwright testing tool, while my teammates handled the backend. We had regular sync-ups to ensure our components integrated smoothly. This collaboration not only helped us meet our deadlines but also improved the overall quality of the product.`,
+    description: (
+      <Box>
+        <Trans>
+          I worked on a project where we had to develop a <b>complex admin
+          dashboard</b> for the company. I was responsible for the frontend using 
+          <b>React</b> and <b>Chakra UI</b> and wrote tests using the <b>Playwright</b> testing tool,
+          while my teammates handled the backend. We had regular sync-ups to
+          ensure our components integrated <b>smoothly</b>. This collaboration not only
+          helped us <b>meet our deadlines</b> but also <b>improved</b> the overall quality of
+          the product.
+        </Trans>
+      </Box>
+    ),
   },
   {
     title: t`6. Tell me about a time when you had to learn something new quickly.`,
-    description: t`When we decided to switch to TypeScript at one of the companies I worked for, I had to get up to speed quickly as I was one of the key developers on the team. I took an online course, read the documentation, and started refactoring small parts of our codebase. Within a few weeks, I was comfortable enough to lead the transition for our entire frontend team.`,
+    description: (
+      <Box>
+        <Trans>
+          When we decided to switch to <b>TypeScript</b> at one of the companies I
+          worked for, I had to get up to speed quickly as I was one of the <b>key
+          developers</b> on the team. I took an online course, read the
+          documentation, and started <b>refactoring</b> small parts of our codebase.
+          Within a <b>few weeks</b>, I was comfortable enough to lead the transition
+          for our entire frontend team.
+        </Trans>
+      </Box>
+    ),
   },
   {
     title: t`7. How do you prioritize your tasks when you have multiple deadlines?`,
-    description: t`I use a combination of prioritization techniques like the Eisenhower Matrix to categorize tasks by urgency and importance. I also break down larger tasks into smaller, manageable chunks and use tools like Trello or Notion to keep track of progress. This helps me stay organized and ensures that I focus on the most critical tasks first.`,
+    description: (
+      <Box>
+        <Trans>
+          I use a combination of prioritization techniques like the <b>Eisenhower
+          Matrix</b> to categorize tasks by urgency and importance. I also break
+          down larger tasks into smaller, manageable chunks and use tools like 
+          <b>Trello</b> or <b>Notion</b> to keep track of progress. This helps me <b>stay
+          organized</b> and ensures that I focus on the most critical tasks first.
+        </Trans>
+      </Box>
+    ),
   },
   {
     title: t`8. A time when you optimized an application for better performance.`,
-    description: t`We had a data-heavy application that was experiencing slow load times. I optimized the code by implementing lazy loading for components and using React.memo to prevent unnecessary re-renders. On the backend, I optimized our database queries and introduced indexing. These changes improved the application's performance significantly, reducing load times by about 40%.`,
+    description: (
+      <Box>
+        <Trans>
+          We had a data-heavy application that was experiencing slow load times.
+          I <b>optimized</b> the code by implementing lazy loading for components and
+          using <b>React.memo</b> to prevent unnecessary re-renders. On the backend, I
+          optimized our database queries and introduced <b>indexing</b>. These changes 
+          <b>improved</b> the application's <b>performance</b> significantly, reducing load
+          times by about <b>40%</b>.
+        </Trans>
+      </Box>
+    ),
   },
   {
     title: t`9. Can you walk me through a project you worked on?`,
     description: (
       <Box>
         <Trans>
-          I worked on an e-commerce platform where we used React, TypeScript,
-          and Chakra UI for the frontend, Next.js for the backend, MongoDB and
-          Prisma for the database, and Playwright for writing tests. I
-          implemented features like user authentication, product listings, and a
-          checkout page. Using TypeScript, I ensured type safety and reduced
+          I worked on an e-commerce platform where we used <b>React</b>, <b>TypeScript</b>,
+          and <b>Chakra UI</b> for the frontend, <b>Next.js</b> for the backend, <b>MongoDB</b> and 
+          <b>Prisma</b> for the database, and <b>Playwright</b> for writing tests. I
+          implemented features like user <b>authentication</b>, <b>product listings</b>, and a 
+          <b>checkout page</b>. Using TypeScript, I ensured type safety and reduced
           bugs. We also used Chakra UI to create a cohesive and responsive
           design, which made the site very user-friendly.
           <br />
-          <br /> Additionally, I collaborated closely with backend developers to
-          integrate the frontend with the server-side logic seamlessly,
+          <br /> Additionally, I <b>collaborated</b> closely with backend developers to
+          integrate the frontend with the server-side logic <b>seamlessly</b>,
           resulting in a smooth and efficient user experience. The project was a
-          great success, improving user engagement and increasing sales for the
+          great <b>success</b>, improving user engagement and <b>increasing sales</b> for the
           platform.
         </Trans>
       </Box>
@@ -87,12 +170,20 @@ const items: FAQItem[] = [
   },
   {
     title: t`10. What do you like to do on your free time?`,
-    description: t`I love hiking, exploring new places and photographing landscapes, which helps me stay creative and refreshed. I also enjoy working on side projects that involve enginnering.`,
+    description: (
+      <Box>
+        <Trans>
+          I love <b>hiking</b>, <b>exploring</b> new places and <b>photographing</b> landscapes,
+          which helps me stay <b>creative</b> and refreshed. I also enjoy working on
+          side projects that involve <b>engineering</b>.
+        </Trans>
+      </Box>
+    ),
   },
-]
+];
 
 const FAQ: FC = () => {
-  const isMobile = useBreakpointValue({ base: true, md: false })
+  const isMobile = useBreakpointValue({ base: true, md: false });
 
   return (
     <FadeInView>
@@ -124,7 +215,7 @@ const FAQ: FC = () => {
         </VStack>
       </Box>
     </FadeInView>
-  )
-}
+  );
+};
 
-export default FAQ
+export default FAQ;
