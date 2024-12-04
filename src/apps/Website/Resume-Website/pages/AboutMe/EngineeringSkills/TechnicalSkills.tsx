d183eaf8b633ea85dfe281f33f960ef74a830404
Ejmen Ramic <ejmenramic5@gmail.com>
2024-12-04 14:21:15 +0800

    Shadow and zoom in added + added popover

--- src/apps/Website/Resume-Website/pages/AboutMe/EngineeringSkills/TechnicalSkills.tsx
+++ src/apps/Website/Resume-Website/pages/AboutMe/EngineeringSkills/TechnicalSkills.tsx
@@ -6,6 +6,13 @@ import {
   useColorModeValue,
   Icon,
   HStack,
+  Popover,
+  PopoverTrigger,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverHeader,
+  PopoverBody,
+  PopoverContent,
 } from '@chakra-ui/react';
 import { Trans } from '@lingui/macro';
 import { FC } from 'react';
@@ -16,6 +23,14 @@ import FadeInView from '../../../../../../shared/components/Hooks/FadeInView';
 const TechnicalSkills: FC = () => {
   const GridColor = useColorModeValue(colors.white, 'gray.800');
   const StackColor = useColorModeValue(colors.white, '#2D3748');
+
+  const hoverShadowLight = useColorModeValue(
+    '0 8px 16px rgba(0, 0, 0, 0.2)',
+    '0 12px 24px rgba(0, 0, 0, 0.3)'
+  );
+
+  const transformScale = useColorModeValue('scale(1)', 'scale(1.03)');
+
   return (
     <FadeInView>
       <Stack
@@ -39,34 +54,48 @@ const TechnicalSkills: FC = () => {
         >
           {itemsTechSkills.map((item, i) => (
             <FadeInView key={i} delay={0.2}>
-              <Stack
-                w={'full'}
-                minH={'190px'}
-                bg={StackColor}
-                spacing={'20px'}
-                p={'32px'}
-                border={'1px solid #ECEFF4'}
-                borderRadius={'10px'}
-                boxShadow={'10px'}
-                transition={'box-shadow 0.3s ease'}
-                _hover={{
-                  boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.1)',
-                }}
-              >
-                <HStack w={'full'} justify={'space-between'}>
-                  <FadeInView delay={0.3}>
-                    <Heading size={'md'}>{item.title}</Heading>
-                  </FadeInView>
+              <Popover>
+                <PopoverTrigger>
+                  <Stack
+                    as={'button'}
+                    w={'full'}
+                    minH={'190px'}
+                    bg={StackColor}
+                    spacing={'20px'}
+                    p={'32px'}
+                    border={'1px solid #ECEFF4'}
+                    borderRadius={'10px'}
+                    boxShadow={'0 8px 12px rgba(0, 0, 0, 0.1)'}
+                    transition={'box-shadow 0.3s ease, transform 0.3s ease'}
+                    _hover={{
+                      boxShadow: hoverShadowLight,
+                      transform: transformScale,
+                    }}
+                  >
+                    <HStack w={'full'} justify={'space-between'}>
+                      <FadeInView delay={0.3}>
+                        <Heading size={'md'}>{item.title}</Heading>
+                      </FadeInView>
 
-                  <FadeInView delay={0.3} direction={'right'}>
-                    <Icon fontSize={'22px'}>{item.icon}</Icon>
-                  </FadeInView>
-                </HStack>
+                      <FadeInView delay={0.3} direction={'right'}>
+                        <Icon fontSize={'22px'}>{item.icon}</Icon>
+                      </FadeInView>
+                    </HStack>
 
-                <FadeInView delay={0.3}>
-                  <Text>{item.detail}</Text>
-                </FadeInView>
-              </Stack>
+                    <FadeInView delay={0.3}>
+                      <Text>{item.detail}</Text>
+                    </FadeInView>
+                  </Stack>
+                </PopoverTrigger>
+                <PopoverContent>
+                  <PopoverArrow />
+                  <PopoverCloseButton />
+                  <PopoverHeader>Confirmation!</PopoverHeader>
+                  <PopoverBody>
+                    Are you sure you want to have that milkshake?
+                  </PopoverBody>
+                </PopoverContent>
+              </Popover>
             </FadeInView>
           ))}
         </Grid>
