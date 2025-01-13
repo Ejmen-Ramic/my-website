b3ba6455825fc542b9e6b8add05641e2bf8f1ea2
Ejmen <ejmenramic5@gmail.com>
2025-01-13 11:31:06 +0800

    Update on playwright

--- src/playwright/tests/home.spec.ts
+++ src/playwright/tests/home.spec.ts
@@ -4,7 +4,7 @@ test.describe('Home Page', () => {
   // This will direct to the home page
   test('go to home', async ({ page }) => {
     // Go to the home page
-    await page.goto('https://localhost:3000/')
+    await page.goto('localhost:3000/')
 
     // Verify the page loaded successfully
     await expect(page).toHaveURL('https://localhost:3000/')
