18afc3bef2fc5ee656335bab522b6e8bca5b4e5f
Ejmen <ejmenramic5@gmail.com>
2025-01-13 10:56:17 +0800

    First test run

--- src/playwright/tests/home.spec.ts
+++ src/playwright/tests/home.spec.ts
@@ -1,5 +1,12 @@
-import test from '@playwright/test'
+import test, { expect } from '@playwright/test'
 
-test('home page', async ({ page }) => {
-  await page.goto('/')
+test.describe('Home Page', () => {
+  // This will direct to the home page
+  test('go to home', async ({ page }) => {
+    // Go to the home page
+    await page.goto('https://localhost:3000/')
+
+    // Verify the page loaded successfully
+    await expect(page).toHaveURL('https://localhost:3000/')
+  })
 })
