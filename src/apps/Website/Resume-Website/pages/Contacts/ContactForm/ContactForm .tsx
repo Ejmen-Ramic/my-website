f166b759111e9ab6e77513b27cb6512bfa1c3a74
Ejmen <ejmenramic5@gmail.com>
2025-01-30 18:31:49 +0800

    - 8

--- src/apps/Website/Resume-Website/pages/Contacts/ContactForm/ContactForm .tsx
+++ src/apps/Website/Resume-Website/pages/Contacts/ContactForm/ContactForm .tsx
@@ -54,9 +54,9 @@ const ContactForm: FC = () => {
   const [message, setMessage] = useState<string>('')
   const toast = useToast()
 
-  useEffect(() => {
-    emailjs.init(process.env.REACT_APP_EMAILJS_PUBLIC_KEY as string)
-  }, [])
+  // useEffect(() => {
+  //   emailjs.init(process.env.REACT_APP_EMAILJS_PUBLIC_KEY as string)
+  // }, [])
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
