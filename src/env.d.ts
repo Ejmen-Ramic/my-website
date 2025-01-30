5e510545bd4627a1dd05841e6139e7686d15ff09
Ejmen <ejmenramic5@gmail.com>
2025-01-30 18:13:16 +0800

    - 6

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,9 +6,9 @@ declare namespace NodeJS {
     PORT?: string
     NODE_ENV: 'development' | 'production' | 'test'
 
-    //Email-JS
-    // REACT_APP_EMAILJS_SERVICE_ID: string
-    // REACT_APP_EMAILJS_TEMPLATE_ID: string
-    // REACT_APP_EMAILJS_PUBLIC_KEY: string
+    // Email-JS
+    REACT_APP_EMAILJS_SERVICE_ID: string
+    REACT_APP_EMAILJS_TEMPLATE_ID: string
+    REACT_APP_EMAILJS_PUBLIC_KEY: string
   }
 }
