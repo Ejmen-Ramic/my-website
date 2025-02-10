f8d32302d5317ee6eeb12a172adf9eb7403f3681
Ejmen <ejmenramic5@gmail.com>
2025-02-10 13:53:10 +0800

    Chakra ui V3 setup + Provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,24 +3,24 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { ChakraProvider, theme } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
 import { i18n } from '@lingui/core'
 import { I18nProvider } from '@lingui/react'
 import { messages } from './locales/en/messages'
+import { Provider } from './components/ui/provider'
 
 i18n.load('en', messages)
 i18n.activate('en')
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
+    <Provider>
       <BrowserRouter>
         <I18nProvider i18n={i18n}>
           <App />
         </I18nProvider>
       </BrowserRouter>
-    </ChakraProvider>
+    </Provider>
   </React.StrictMode>
 )
 
