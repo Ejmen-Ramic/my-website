0b4e297d5540320b6bc445927986f9a36c641589
Ejmen <ejmenramic5@gmail.com>
2025-02-24 23:02:05 +0800

    AboutCarousel organization 2

--- src/apps/Website/Resume-Website/pages/AboutMe/index.tsx
+++ src/apps/Website/Resume-Website/pages/AboutMe/index.tsx
@@ -1,5 +1,5 @@
 import { VStack } from '@chakra-ui/react'
-import AboutIntro from './Intro/AboutIntro'
+import AboutIntro from './Intro'
 // import SchoolCertificates from './SchoolCertificates'
 // import OurProgress from './OurProgress/OurProgress'
 import Milestones from './Milestone'
