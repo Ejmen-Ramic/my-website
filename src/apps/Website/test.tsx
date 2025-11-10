b6d7916af446b9d60ac3c2cb217ed382adcf08f2
Ejmen Ramic <ejmenramic5@gmail.com>
2025-11-10 10:12:03 +0800

    .

--- src/apps/Website/test.tsx
+++ src/apps/Website/test.tsx
@@ -62,7 +62,7 @@ const Test = () => {
           {editingIndex !== null ? 'Update Task' : 'Add Task'}
         </Button>
         <Button colorScheme='red' onClick={handleClearAll}>
-          Clear Al
+          Clear All
         </Button>
       </HStack>
       <Text>Amount of movies added: {addItems.length} </Text>
