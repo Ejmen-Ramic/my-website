17ddd5cc8dc9346bfcd325f06e86e5bf583799d0
Ejmen Ramic <ejmenramic5@gmail.com>
2025-12-27 09:48:55 +0800

    .

--- src/apps/Website/test.tsx
+++ src/apps/Website/test.tsx
@@ -1,48 +1,7 @@
-import { Button, Input, Stack, Text } from '@chakra-ui/react';
-import { count } from 'console';
-import { useEffect, useState } from 'react';
+import { Stack } from '@chakra-ui/react';
 
 const Test = () => {
-  const [isSaving, setIsSaving] = useState(false);
-  const [countdown, setCountdown] = useState(3);
-  const [email, setEmail] = useState('');
-
-  const handleSave = () => {
-    setIsSaving(true);
-    setCountdown(3);
-  };
-
-  useEffect(() => {
-    if (!isSaving) return;
-
-    if (countdown === 0) {
-      setIsSaving(false);
-      return;
-    }
-
-    const timer = setTimeout(() => {
-      setCountdown((prev) => prev - 1);
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, [isSaving, countdown]);
-
-  return (
-    <Stack>
-      <Input
-        placeholder='type your email'
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-
-      <Button onClick={handleSave} isLoading={isSaving}>
-        Subscribe
-      </Button>
-
-      {isSaving && (
-        <Text>Subscribed successfully ✅ (disappears in {countdown})</Text>
-      )}
-    </Stack>
-  );
+  return <Stack></Stack>;
 };
 
 export default Test;
