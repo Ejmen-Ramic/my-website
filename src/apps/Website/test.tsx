2cdc1d4d67030fb0e496f6225f2dd30b9d1680fe
Ejmen Ramic <ejmenramic5@gmail.com>
2026-01-02 10:48:26 +0800

    .

--- src/apps/Website/test.tsx
+++ src/apps/Website/test.tsx
@@ -12,74 +12,7 @@ import {
 import { useEffect, useState } from 'react';
 
 const Test = () => {
-  const [form, setForm] = useState({
-    name: '',
-    theme: 'light',
-    emailNotification: false,
-    isSaving: false,
-  });
-
-  const handleForm = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  ) => {
-    const { name, value } = e.target;
-    setForm((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  const handleCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { checked } = e.target;
-    setForm((prev) => ({
-      ...prev,
-      emailNotification: checked,
-    }));
-  };
-
-  const handleSave = () => {
-    setForm((prev) => ({
-      ...prev,
-      isSaving: true,
-    }));
-  };
-
-  const isDisabled = form.name.trim() === '' || form.isSaving;
-
-  useEffect(() => {
-    if (!form.isSaving) return;
-
-    const timer = setTimeout(() => {
-      setForm((prev) => ({
-        ...prev,
-        isSaving: false,
-      }));
-    }, 2000);
-    return () => clearTimeout(timer);
-  }, [form.isSaving]);
-
-  return (
-    <VStack>
-      <Input name={'name'} value={form.name} onChange={handleForm} />
-      <Select name={'theme'} value={form.theme} onChange={handleForm}>
-        <option value={'light'}>Lights</option>
-        <option value={'dark'}>Dark</option>
-      </Select>
-      <Checkbox isChecked={form.emailNotification} onChange={handleCheck}>
-        Email Notification
-      </Checkbox>
-      <Button
-        loadingText={'loading'}
-        onClick={handleSave}
-        isLoading={form.isSaving}
-        isDisabled={isDisabled}
-      >
-        Save
-      </Button>
-
-      {form.isSaving && <Text>Profile saved ✅</Text>}
-    </VStack>
-  );
+  return <VStack></VStack>;
 };
 
 export default Test;
