b507b96ceabedd61a79c6ce86834c40c582bbea9
Ejmen Ramic <ejmenramic5@gmail.com>
2026-01-07 11:07:43 +0800

    .

--- src/apps/Website/test.tsx
+++ src/apps/Website/test.tsx
@@ -12,7 +12,35 @@ import {
 import { useEffect, useState } from 'react';
 
 const Test = () => {
-  return <VStack>Test</VStack>;
+  const [form, setForm] = useState({
+    username: '',
+  });
+
+  const handleForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const canSubmit = form.username.trim() !== '';
+
+  return (
+    <VStack>
+      <Input
+        name='username'
+        placeholder={'username'}
+        value={form.username}
+        onChange={handleForm}
+      />
+      <Button isDisabled={!canSubmit}>Submit</Button>
+      <Text color={canSubmit ? 'green' : 'red'}>
+        {canSubmit ? 'Ready' : 'Not Ready'}
+      </Text>
+    </VStack>
+  );
 };
 
 export default Test;
