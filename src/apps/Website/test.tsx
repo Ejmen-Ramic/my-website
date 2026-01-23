8292c213e2aaae1344dfdb23792b5317984f9748
Ejmen Ramic <ejmenramic5@gmail.com>
2026-01-23 22:24:23 +0800

    .

--- src/apps/Website/test.tsx
+++ src/apps/Website/test.tsx
@@ -13,28 +13,27 @@ import {
   Radio,
   RadioGroup,
 } from '@chakra-ui/react';
-('');
+
 import React, { useEffect, useState } from 'react';
 
 const Test = () => {
   const [value, setValue] = useState('');
   const [touched, setTouched] = useState(false);
+  const [dirty, setDirty] = useState(false);
 
   return (
-    <Flex w={'100vw'} align={'center'} justify={'center'}>
-      <VStack w={'300px'} pt={'100px'} spacing={4}>
-        <Input
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          onBlur={() => setTouched(true)}
-          placeholder='Type something'
-        />
-
-        {touched && value.trim() === '' && (
-          <Text color='red'>This field is required</Text>
-        )}
+    <Flex w='100vw' align='center' justify='center'>
+      <VStack w='300px' pt='100px' spacing={4}>
+        <Input placeholder={'type value'} />
+        <Text>Required</Text>
+        <Text>Too short</Text>
+        <Text>Looks good</Text>
+        <Text fontSize='sm'>
+          touched: {String(touched)} | dirty: {String(dirty)}
+        </Text>
       </VStack>
     </Flex>
   );
 };
+
 export default Test;
