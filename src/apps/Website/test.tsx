29aec8f8049f4570d814f2c3c7bbd37650ee39e7
Ejmen Ramic <ejmenramic5@gmail.com>
2026-03-06 11:45:39 +0800

    .

--- src/apps/Website/test.tsx
+++ src/apps/Website/test.tsx
@@ -18,60 +18,9 @@ import {
 import React, { useEffect, useState } from 'react';
 
 const Test = () => {
-  const [form, setForm] = useState({
-    username: '',
-    email: '',
-  });
-
-  const [touchedUsername, setTouchedUsername] = useState<boolean>(false);
-  const [touchedEmail, setTouchedEmail] = useState<boolean>(false);
-
-  const canSubmit = form.username.length >= 3 && form.email.includes('@');
-
-  const handleForm = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setForm((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
   return (
     <Flex w='100vw' align='center' justify='center'>
-      <VStack w='300px' pt='100px' spacing={4}>
-        <Input
-          name='username'
-          value={form.username}
-          onChange={handleForm}
-          onBlur={() => setTouchedUsername(true)}
-          placeholder={'Type Username'}
-        />
-        <Input
-          name='email'
-          value={form.email}
-          onChange={handleForm}
-          onBlur={() => setTouchedEmail(true)}
-          placeholder={'Type Email'}
-        />
-        {touchedUsername && form.username.trim() === '' && (
-          <Text color={'red'}>User is required</Text>
-        )}
-        {touchedUsername &&
-          form.username.trim() !== '' &&
-          form.username.length < 3 && (
-            <Text>Username must be at least 2 characters</Text>
-          )}
-
-        {touchedEmail && form.email.trim() === '' && (
-          <Text color={'red'}>Email is required</Text>
-        )}
-        {touchedEmail &&
-          form.email.trim() !== '' &&
-          !form.email.includes('@') && (
-            <Text color='red'>Email must include @</Text>
-          )}
-        <Button isDisabled={!canSubmit}></Button>
-      </VStack>
+      <VStack w='300px' pt='100px' spacing={4}></VStack>
     </Flex>
   );
 };
