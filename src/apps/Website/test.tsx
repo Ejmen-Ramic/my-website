cd2d18ad85838c1053430adf012b7fcd0af03875
Ejmen Ramic <ejmenramic5@gmail.com>
2026-03-13 00:54:21 +0800

    .

--- src/apps/Website/test.tsx
+++ src/apps/Website/test.tsx
@@ -3,7 +3,9 @@ import { VStack, Flex } from '@chakra-ui/react';
 const Test = () => {
   return (
     <Flex w='100vw' align='center' justify='center'>
-      <VStack w='300px' pt='100px' spacing={4}></VStack>
+      <VStack w='300px' pt='100px' spacing={4}>
+        test
+      </VStack>
     </Flex>
   );
 };
