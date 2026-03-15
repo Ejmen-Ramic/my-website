4eaa4437c96a6443c4bce73f006e29013c51a130
Ejmen Ramic <ejmenramic5@gmail.com>
2026-03-16 04:08:19 +0800

    .

--- src/apps/Website/test.tsx
+++ src/apps/Website/test.tsx
@@ -20,7 +20,9 @@ import React, { useEffect, useState } from 'react';
 const Test = () => {
   return (
     <Flex w='100vw' align='center' justify='center'>
-      <VStack w='300px' pt='100px' spacing={4}></VStack>
+      <VStack w='300px' pt='100px' spacing={4}>
+        test
+      </VStack>
     </Flex>
   );
 };
