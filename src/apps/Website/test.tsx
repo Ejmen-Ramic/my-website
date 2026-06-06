25b0a844d5cb98eeb05ffa94c23444264b64cd2f
Ejmen Ramic <ejmenramic5@gmail.com>
2026-06-06 13:57:52 +0800

    .

--- src/apps/Website/test.tsx
+++ src/apps/Website/test.tsx
@@ -1,12 +1,19 @@
-import { VStack, Flex } from '@chakra-ui/react';
+import { Stack, VStack, Text, HStack, Button, Input } from '@chakra-ui/react';
 
 const Test = () => {
   return (
-    <Flex w='100vw' align='center' justify='center'>
-      <VStack w='300px' pt='100px' spacing={4}>
-        tests
-      </VStack>
-    </Flex>
+    <VStack w={'full'} mt={'300px'}>
+      <Stack
+        w={'full'}
+        maxW={'400px'}
+        borderRadius={'md'}
+        border={'1px solid'}
+        borderColor={'gray.200'}
+        p={4}
+      >
+        <Input />
+      </Stack>
+    </VStack>
   );
 };
 
