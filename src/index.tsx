6d963fffb67a43f339316a0efc416b4a519c45bd
Ejmen Ramic <ejmenramic5@gmail.com>
2026-06-10 10:54:17 +0800

    error fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import { ChakraProvider, theme } from '@chakra-ui/react'
-import { BrowserRouter } from 'react-router-dom'
-import { i18n } from '@lingui/core'
-import { I18nProvider } from '@lingui/react'
-import { messages } from './locales/en/messages'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+import { i18n } from '@lingui/core';
+import { I18nProvider } from '@lingui/react';
+import { messages } from './locales/en/messages';
 
-i18n.load('en', messages)
-i18n.activate('en')
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+i18n.load('en', messages);
+i18n.activate('en');
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
@@ -21,10 +22,10 @@ root.render(
         </I18nProvider>
       </BrowserRouter>
     </ChakraProvider>
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
