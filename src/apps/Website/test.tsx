098477f94ab798169d2ac0b09aa9ecb34450961e
Ejmen Ramic <ejmenramic5@gmail.com>
2026-06-16 18:41:48 +0800

    temperature converter

--- src/apps/Website/test.tsx
+++ src/apps/Website/test.tsx
@@ -1,86 +1,60 @@
-import {
-  Box,
-  Button,
-  Text,
-  Input,
-  VStack,
-  HStack,
-  Stack,
-} from '@chakra-ui/react';
-import { FC, useCallback, useEffect, useMemo, useState } from 'react';
-
-interface CartItem {
-  id: number;
-  title: string;
-  price: number;
-  quantity: number;
-}
-
-const products: CartItem[] = [
-  { id: 1, title: 'Bread', price: 3, quantity: 0 },
-  { id: 2, title: 'Cereal', price: 5, quantity: 0 },
-  { id: 3, title: 'Meat', price: 10, quantity: 0 },
-  { id: 4, title: 'Fruits', price: 20, quantity: 0 },
-];
+import { Button, HStack, VStack, Text, Input } from '@chakra-ui/react';
+import { FC, useMemo, useState } from 'react';
 
 const Test: FC = () => {
-  const [cart, setCart] = useState<CartItem[]>([]);
+  const [celsius, setCelsius] = useState(0);
 
-  const handleAddToCart = (product: CartItem) => {
-    const existing = cart.find((item) => item.id === product.id);
-    if (existing) {
-      setCart(
-        cart.map((item) => {
-          if (item.id === product.id) {
-            return { ...item, quantity: item.quantity + 1 };
-          }
-          return item;
-        }),
-      );
+  const handleReset = () => {
+    setCelsius(0);
+  };
+
+  const tempConverter = useMemo(() => {
+    return (celsius * 9) / 5 + 32;
+  }, [celsius]);
+
+  const message = useMemo(() => {
+    if (tempConverter > 100) {
+      return 'Hot!';
+    } else if (tempConverter < 32) {
+      return 'Freezing';
     } else {
-      setCart([...cart, { ...product, quantity: 1 }]);
+      return '';
     }
-  };
-
-  const handleDeleteAll = () => {
-    setCart([]);
-  };
-
-  const handleRemoveItem = (product: CartItem) => {
-    setCart(cart.filter((item) => item.id !== product.id));
-  };
-
-  const totalSum = useMemo(
-    () => cart.reduce((total, item) => total + item.price * item.quantity, 0),
-    [cart],
-  );
-
-  const totalQuantity = useMemo(
-    () => cart.reduce((total, item) => total + item.quantity, 0),
-    [cart],
-  );
+  }, [tempConverter]);
 
   return (
-    <VStack w={'full'} alignContent={'center'} pt={'200px'}>
-      <Button onClick={handleDeleteAll}>Delete All</Button>
-      {products.map((product) => (
-        <HStack w={'full'} maxW={'300px'} key={product.id}>
-          <Text>{product.title}</Text>
-          <Button onClick={() => handleAddToCart(product)}>Add to cart</Button>
-        </HStack>
-      ))}
-      {cart.map((item) => (
-        <VStack key={item.id}>
-          <HStack>
-            <Text>{item.title}</Text>
-            <Text>{item.price}</Text>
-            <Text>{item.quantity}</Text>
-            <Button onClick={() => handleRemoveItem(item)}>Remove</Button>
-          </HStack>
-        </VStack>
-      ))}
-      <Text>Total summed price: {totalSum}</Text>
-      <Text>Total amount of items: {totalQuantity}</Text>
+    <VStack>
+      <HStack>
+        <Input
+          value={celsius}
+          onChange={(e) => setCelsius(Number(e.target.value))}
+        />
+      </HStack>
+      <HStack>
+        <Text
+          color={
+            tempConverter > 100
+              ? 'red'
+              : tempConverter < 32
+              ? 'lightblue'
+              : 'green'
+          }
+        >
+          {tempConverter}
+        </Text>
+        <Text
+          color={
+            tempConverter > 100
+              ? 'red'
+              : tempConverter < 32
+              ? 'lightblue'
+              : 'green'
+          }
+        >
+          {celsius}
+        </Text>
+        <Button onClick={handleReset}>Reset</Button>
+      </HStack>
     </VStack>
   );
 };
