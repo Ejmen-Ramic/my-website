9fe2522b2f302c05824dcd8ef09a25424d6bdecf
Ejmen Ramic <ejmenramic5@gmail.com>
2026-06-18 00:22:10 +0800

    split bill practice

--- src/apps/Website/test.tsx
+++ src/apps/Website/test.tsx
@@ -2,38 +2,26 @@ import { Button, Input, VStack, Text } from '@chakra-ui/react';
 import { FC, useMemo, useState } from 'react';
 
 const Test: FC = () => {
-  const [height, setHeight] = useState(0);
-  const [weight, setWeight] = useState(0);
+  const [bill, setBill] = useState(0);
+  const [people, setPeople] = useState(0);
 
-  const BMI = useMemo(() => {
-    return weight / (height / 100) ** 2;
-  }, [height, weight]);
-
-  const Categories = useMemo(() => {
-    if (BMI < 18.5) {
-      return 'UnderWeight';
-    } else if (BMI >= 18.5 && BMI <= 24.9) {
-      return 'Normal';
-    } else if (BMI >= 24.9 && BMI <= 29.9) {
-      return 'Overweight';
-    } else {
-      return 'Obese';
-    }
-  }, [BMI]);
+  const splitBill = useMemo(() => {
+    return people === 0 ? 0 : bill / people;
+  }, [bill, people]);
 
   return (
     <VStack>
       <Input
         type={'number'}
-        value={height}
-        onChange={(e) => setHeight(Number(e.target.value))}
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
       />
       <Input
         type={'number'}
-        value={weight}
-        onChange={(e) => setWeight(Number(e.target.value))}
+        value={people}
+        onChange={(e) => setPeople(Number(e.target.value))}
       />
-      <Text>{Categories}</Text>
+      <Text>{splitBill}</Text>
     </VStack>
   );
 };
