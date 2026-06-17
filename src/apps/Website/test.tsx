d8cdcb8d2ecb227ba415950a97007750bc00ed2b
Ejmen Ramic <ejmenramic5@gmail.com>
2026-06-17 21:43:33 +0800

    Todo app practice

--- src/apps/Website/test.tsx
+++ src/apps/Website/test.tsx
@@ -1,60 +1,77 @@
-import { Button, HStack, VStack, Text, Input } from '@chakra-ui/react';
-import { FC, useMemo, useState } from 'react';
+import { VStack, HStack, Input, Button, Text } from '@chakra-ui/react';
+import { FC, useState } from 'react';
+
+interface TaskProps {
+  title: string;
+  completion: boolean;
+  id: number;
+}
 
 const Test: FC = () => {
-  const [celsius, setCelsius] = useState(0);
+  const [addTask, setAddTask] = useState('');
+  const [tasks, setTasks] = useState<TaskProps[]>([]);
 
-  const handleReset = () => {
-    setCelsius(0);
+  const handleAddTask = () => {
+    const newItem = { title: addTask, completion: false, id: Date.now() };
+    setTasks([...tasks, newItem]);
+    setAddTask('');
   };
 
-  const tempConverter = useMemo(() => {
-    return (celsius * 9) / 5 + 32;
-  }, [celsius]);
+  const handleToggle = (id: number) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, completion: !task.completion };
+        }
+        return task;
+      }),
+    );
+  };
 
-  const message = useMemo(() => {
-    if (tempConverter > 100) {
-      return 'Hot!';
-    } else if (tempConverter < 32) {
-      return 'Freezing';
-    } else {
-      return '';
-    }
-  }, [tempConverter]);
+  const handleDeleteTask = (id: number) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const handleDeleteAll = () => {
+    setTasks([]);
+  };
+
+  const [progress, setProgress] = useState<'all' | 'active' | 'completed'>(
+    'all',
+  );
+
+  let filteredTasks = tasks;
+  if (progress === 'active') {
+    filteredTasks = tasks.filter((task) => !task.completion);
+  } else if (progress === 'completed') {
+    filteredTasks = tasks.filter((task) => task.completion);
+  }
 
   return (
-    <VStack>
-      <HStack>
-        <Input
-          value={celsius}
-          onChange={(e) => setCelsius(Number(e.target.value))}
-        />
-      </HStack>
-      <HStack>
-        <Text
-          color={
-            tempConverter > 100
-              ? 'red'
-              : tempConverter < 32
-              ? 'lightblue'
-              : 'green'
-          }
-        >
-          {tempConverter}
-        </Text>
-        <Text
-          color={
-            tempConverter > 100
-              ? 'red'
-              : tempConverter < 32
-              ? 'lightblue'
-              : 'green'
-          }
-        >
-          {celsius}
-        </Text>
-        <Button onClick={handleReset}>Reset</Button>
-      </HStack>
+    <VStack w={'full'}>
+      <VStack>
+        <HStack w={'full'}>
+          <Input value={addTask} onChange={(e) => setAddTask(e.target.value)} />
+          <Button onClick={handleAddTask}>Add Task</Button>
+          <Button onClick={handleDeleteAll}>Delete All</Button>
+          <HStack w={'full'}>
+            <Button onClick={() => setProgress('all')}>All</Button>
+            <Button onClick={() => setProgress('active')}>Active</Button>
+            <Button onClick={() => setProgress('completed')}>Completed</Button>
+          </HStack>
+        </HStack>
+        {filteredTasks.map((task) => (
+          <HStack key={task.id}>
+            <Text>{task.title}</Text>
+            <Text color={task.completion ? 'green' : 'red'}>
+              {task.completion ? 'completed' : 'not completed'}
+            </Text>
+
+            <Button onClick={() => handleToggle(task.id)}>Toggle</Button>
+            <Button onClick={() => handleDeleteTask(task.id)}>Delete</Button>
+          </HStack>
+        ))}
+      </VStack>
     </VStack>
   );
 };
