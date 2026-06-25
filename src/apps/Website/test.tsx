efaeed96d01509dee0f4ceb01690466c73f59045
Ejmen Ramic <ejmenramic5@gmail.com>
2026-06-25 15:04:03 +0800

    task with completion practice

--- src/apps/Website/test.tsx
+++ src/apps/Website/test.tsx
@@ -10,46 +10,46 @@ import {
 import { title } from 'process';
 import { FC, useMemo, useState } from 'react';
 
-const people = ['Alice', 'Bob', 'Charlie', 'Diana'];
-
-interface ExpenseProps {
+interface Props {
   id: number;
-  description: string;
-  amount: number;
-  paidBy: string;
+  title: string;
+  completed: boolean;
 }
 
 const Test: FC = () => {
-  const [inputDescription, setInputDescription] = useState('');
-  const [inputAmount, setInputAmount] = useState(0);
-  const [expenses, setExpenses] = useState<ExpenseProps[]>([]);
-  const [paidBy, setPaidBy] = useState('');
-
-  const handleAddExpense = () => {
-    if (inputDescription === '' || paidBy === '') return;
-    const existing = {
-      id: Date.now(),
-      description: inputDescription,
-      amount: inputAmount,
-      paidBy: paidBy,
-    };
-    setExpenses([...expenses, existing]);
-    setInputAmount(0);
-    setPaidBy('');
-    setInputDescription('');
-  };
+  const [tasks, setTasks] = useState<Props[]>([]);
+  const [addTask, setAddTask] = useState('');
 
   const handleRemoveAll = () => {
-    setExpenses([]);
+    setTasks([]);
   };
 
-  const handleRemoveItem = (id: number) => {
-    setExpenses(expenses.filter((item) => item.id !== id));
+  const handleRemoveTask = (id: number) => {
+    setTasks(tasks.filter((task) => task.id !== id));
   };
 
-  const totalExpense = useMemo(() => {
-    return expenses.reduce((total, item) => total + item.amount, 0);
-  }, [expenses]);
+  const handleAddTask = () => {
+    const newItem = { id: Date.now(), title: addTask, completed: false };
+    setTasks([...tasks, newItem]);
+    setAddTask('');
+  };
+
+  const handleToggle = (id: number) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: !task.completed };
+        }
+        return task;
+      }),
+    );
+  };
+
+  const completionRate = useMemo(() => {
+    if (tasks.length === 0) return 0;
+    const completedCount = tasks.filter((task) => task.completed).length;
+    return (completedCount / tasks.length) * 100;
+  }, [tasks]);
 
   return (
     <VStack w={'full'} alignContent={'center'} mt={'300px'}>
@@ -65,39 +65,24 @@ const Test: FC = () => {
       >
         <Button onClick={handleRemoveAll}> Remove All</Button>
         <HStack w={'full'} maxW={'500px'} justifyContent={'space-between'}>
-          <Input
-            value={inputDescription}
-            onChange={(e) => setInputDescription(e.target.value)}
-          />
-          <Input
-            type={'number'}
-            value={inputAmount}
-            onChange={(e) => setInputAmount(Number(e.target.value))}
-          />
-          <Select value={paidBy} onChange={(e) => setPaidBy(e.target.value)}>
-            <option value={''}>Select person</option>
-            {people.map((person) => (
-              <option key={person} value={person}>
-                {person}
-              </option>
-            ))}
-          </Select>
-          <Button onClick={handleAddExpense}>Add Expense</Button>
+          <Input value={addTask} onChange={(e) => setAddTask(e.target.value)} />
+          <Button onClick={handleAddTask}>Add Task</Button>
         </HStack>
 
         <VStack w={'full'} maxW={'400px'}>
-          <HStack gap={'30px'}>
-            {expenses.map((item) => (
-              <VStack key={item.id}>
-                <Text>Amount: {item.amount}</Text>
-                <Text>Description: {item.description}</Text>
-                <Text>Paid by: {item.paidBy}</Text>
-                <Button onClick={() => handleRemoveItem(item.id)}>
-                  Remove
-                </Button>
-              </VStack>
-            ))}
-          </HStack>
+          {tasks.map((task) => (
+            <HStack gap={'30px'} key={task.id}>
+              <Button onClick={() => handleToggle(task.id)}>T</Button>
+              <Text color={task.completed ? 'Green' : 'Red'}>
+                {task.completed ? 'Completed' : 'Not Completed'}
+              </Text>
+              <Text>{task.title}</Text>
+              <Button onClick={() => handleRemoveTask(task.id)}>
+                Remove Task
+              </Button>
+            </HStack>
+          ))}
+          <Text>{completionRate}%</Text>
         </VStack>
       </VStack>
     </VStack>
